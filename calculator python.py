ff20030152a9ba60ad19f28595be4991a798e10f
Ananya1603 <140078911+Ananya1603@users.noreply.github.com>
2024-07-12 19:10:29 +0530

    Add files via upload

--- calculator python.py
+++ calculator python.py
@@ -29,7 +29,7 @@ while True:
         elif choice == '3':
             print(num1,'*',num2, multiply(num1,num2))
 
-        elif choice == '1':
+        elif choice == '4':
             print(num1,'/',num2, divide(num1,num2))
 
         break
